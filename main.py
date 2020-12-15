354eb35de47cf8d45751f9b9d7ba4c71e4106ae7
Dariusz Okonski <darek200180@gmail.com>
2020-12-15 13:29:51 +0000

    add functions to read and write data to file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter
-from tkinter import END
+from tkinter import END, ANCHOR
 
 root = tkinter.Tk()
 root.title('Simple Checklist')
@@ -17,6 +17,29 @@ root.config(bg=root_color)
 def add_item():
     my_listbox.insert(END, list_entry.get())
     list_entry.delete(0, END)
+
+def remove_item():
+    my_listbox.delete(ANCHOR)
+
+def clear_list():
+    my_listbox.delete(0, END)
+
+def save_list():
+    with open('checklist.txt', 'w') as f:
+        list_tuple = my_listbox.get(0, END)
+        for item in list_tuple:
+            if item.endswith('\n'):
+                f.write(item)
+            else:
+                f.write(item + '\n')
+
+def open_list():
+    try:
+        with open('checklist.txt', 'r') as f:
+            for line in f:
+                my_listbox.insert(END, line)
+    except:
+        return
 
 # define layout
 # create frames
@@ -45,9 +68,9 @@ my_listbox.grid(row=0, column=0)
 my_scrollbar.grid(row=0, column=1, sticky="NS")
 
 # button frame layout ===========================
-list_remove_button = tkinter.Button(button_frame, text="Remove Item", borderwidth=2, font=my_font, bg=button_color)
-list_clear_button = tkinter.Button(button_frame, text="Clear List", borderwidth=2, font=my_font, bg=button_color)
-save_button = tkinter.Button(button_frame, text="Save List", borderwidth=2, font=my_font, bg=button_color)
+list_remove_button = tkinter.Button(button_frame, text="Remove Item", borderwidth=2, font=my_font, bg=button_color, command=remove_item)
+list_clear_button = tkinter.Button(button_frame, text="Clear List", borderwidth=2, font=my_font, bg=button_color, command=clear_list)
+save_button = tkinter.Button(button_frame, text="Save List", borderwidth=2, font=my_font, bg=button_color, command=save_list)
 quit_button = tkinter.Button(button_frame, text="Quit", borderwidth=2, font=my_font, bg=button_color, command=root.destroy)
 
 list_remove_button.grid(row=0, column=0, padx=2, pady=10)
@@ -60,5 +83,5 @@ quit_button.grid(row=0, column=3, padx=2, pady=10, ipadx=25)
 
 
 
-
+open_list()
 root.mainloop()
